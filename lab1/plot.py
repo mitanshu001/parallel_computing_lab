4bae69b0ab09135000ab6db21c85ce58d02f09db
mitanshu001 <mitanshu.sheo@iitg.ernet.in>
2020-01-29 11:53:59 +0530

    report and plot

--- lab1/plot.py
+++ lab1/plot.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import matplotlib.pyplot as plt
 import csv
 
@@ -7,39 +8,56 @@ t0 = []
 t1 = []
 t2 = []
 t3 = []
+t4 =[]
+
+with open('theoritical.csv','r') as csvfile:
+	plots = csv.reader(csvfile, delimiter=',')
+	for row in plots:
+		# print(row)
+		# n.append(int(row[0]))
+		t4.append(int(row[1]))
+
+
 
 with open('myfile.csv','r') as csvfile:
-    plots = csv.reader(csvfile, delimiter=',')
-    for row in plots:
-        n.append(int(row[0]))
-        t0.append(int(row[1]))
+	plots = csv.reader(csvfile, delimiter=',')
+	for row in plots:
+		# print(row)
+		n.append(int(row[0]))
+		t0.append(int(row[1]))
 
 
 with open('myfile1.csv','r') as csvfile:
-    plots = csv.reader(csvfile, delimiter=',')
-    for row in plots:
-        # n.append(int(row[0]))
-        t1.append(int(row[1]))
+	plots = csv.reader(csvfile, delimiter=',')
+	for row in plots:
+		# n.append(int(row[0]))
+		t1.append(int(row[1]))
 
 with open('myfile2.csv','r') as csvfile:
-    plots = csv.reader(csvfile, delimiter=',')
-    for row in plots:
-        # n.append(int(row[0]))
-        t2.append(int(row[1]))
+	plots = csv.reader(csvfile, delimiter=',')
+	for row in plots:
+		# n.append(int(row[0]))
+		# print(row[1])
+		t2.append(int(row[1]))
+		# print(row[1])
 
-
-with open('myfile3.csv','r') as csvfile:
-    plots = csv.reader(csvfile, delimiter=',')
-    for row in plots:
-        # n.append(int(row[0]))
-        t3.append(int(row[1]))
-
+with open('myfile3_64.csv','r') as csvfile:
+	plots = csv.reader(csvfile, delimiter=',')
+	for row in plots:
+		# n.append(int(row[0]))
+		t3.append(int(row[1]))
+print(t0)
+print(t1)
+print(t2)
+print(t3)
 plt.figure()
 plt.xlabel('Matrix size (n)')
 plt.ylabel('Time in sec')
-plt.plot(n,t0)
-plt.plot(n,t1)
-plt.plot(n,t2)
-plt.plot(n,t3)
-
+plt.plot(n,t0,label="naive")
+plt.plot(n,t1,label="opt1")
+plt.plot(n,t2,label="opt2")
+plt.plot(n,t3,label="opt3")
+plt.plot(n,t4,label="theoritical")
+plt.legend(loc="upper left")
+plt.show()
 # load()
